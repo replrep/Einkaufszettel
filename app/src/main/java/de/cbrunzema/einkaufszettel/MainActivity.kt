fe2ba298186307c547520aede17c01cacde9de6a
Claus Brunzema <mail@cbrunzema.de>
2025-05-11 01:13:44 +0200

    Show two scrollable text lists

--- app/src/main/java/de/cbrunzema/einkaufszettel/MainActivity.kt
+++ app/src/main/java/de/cbrunzema/einkaufszettel/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+
 import de.cbrunzema.einkaufszettel.ui.theme.EinkaufszettelTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+val fontSize = 30.sp
+val lineHeight = 54.sp
+val padding = 10.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             EinkaufszettelTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainView(Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +38,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LeftPanel() {
+    for (i in 0..82) {
+        Text("left $i", fontSize = fontSize, lineHeight = lineHeight)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EinkaufszettelTheme {
-        Greeting("Android")
+fun RightPanel() {
+    for (i in 0..82) {
+        Text("right $i", fontSize = fontSize, lineHeight = lineHeight)
+    }
+}
+
+@Composable
+fun MainView(modifier: Modifier = Modifier) {
+    val leftScrollState = rememberScrollState()
+    val rightScrollState = rememberScrollState()
+    Row(
+        modifier
+            .fillMaxSize()
+            .padding(horizontal = padding)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1.0f)
+                .verticalScroll(leftScrollState)
+        ) {
+            LeftPanel()
+        }
+        Column(
+            modifier = Modifier
+                .weight(1.0f)
+                .verticalScroll(rightScrollState)
+        ) {
+            RightPanel()
+        }
     }
 }
