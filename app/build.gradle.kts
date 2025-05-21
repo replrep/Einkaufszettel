7808505839588afe243b951194ece5b7d5d5e6e8
Claus Brunzema <mail@cbrunzema.de>
2025-05-21 22:58:26 +0200

    reduce minSdk for my old phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "de.cbrunzema.einkaufszettel"
-        minSdk = 31
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "0.0.1"
